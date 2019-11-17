b6ffdae8a6da2a0075e3694197beb64bb4154f7c
yvzydn <yavuzaydinus@gmail.com>
2019-11-17 15:40:30 -0500

    my pet is here

---
+++ src/day33/Pets.java
@@ -0,0 +1,19 @@
+package day33;
+
+public class Pets {
+
+
+    public int legs;
+    public String name;
+    public String color;
+    public int price;
+    public boolean canSwim;
+
+public static String PetName(String name){
+    return name;
+}public static int PetLeg(int num){
+return num;
+}
+}
+
+
